3104178983b7247e32c04872da97aa6663ccf78a
LenLenTM <91597420+LenLenTM@users.noreply.github.com>
2024-02-10 12:00:44 +0100

    fsm pseudo code

---
+++ Assets/Code/FSM_Test.cs
@@ -0,0 +1,64 @@
+﻿public class FiniteStateMachine
+{
+
+    private bool spotPacMan;
+    private bool powerPellet;
+    private bool loseSightOfPacMan;
+    private bool eaten;
+
+    void WanderTheMaze()
+    {
+        while (true)
+        {
+            // wander algorithm
+
+            if (spotPacMan)
+            {
+                ChasePacMan();
+            }
+
+            if (powerPellet)
+            {
+                FleePacMan();
+            }
+        }
+    }
+
+    void ChasePacMan()
+    {
+        // chase algorithm
+
+        if (loseSightOfPacMan)
+        {
+            WanderTheMaze();
+        }
+
+        if (powerPellet)
+        {
+            FleePacMan();
+        }
+    }
+
+    void FleePacMan()
+    {
+        // flee algorithm
+        
+        if (!powerPellet)
+        {
+            WanderTheMaze();
+        }
+
+        if (eaten)
+        {
+            ReturnToBase();
+        }
+    }
+
+    void ReturnToBase()
+    {
+        // return to base algorithm
+        
+        WanderTheMaze();
+    }
+    
+}
